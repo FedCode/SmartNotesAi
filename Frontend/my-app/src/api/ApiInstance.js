c0f695dda13b0fc5199bbdd3d221ef436f76f2c0
Sandeep Kumar <cbitssdesigner@gmail.com>
2026-04-24 16:37:31 +0530

    URL Changes

--- Frontend/my-app/src/api/ApiInstance.js
+++ Frontend/my-app/src/api/ApiInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiInstance  = axios.create({
-    baseURL:"https://smartnotesai-7ifv.onrender.com//api",
+    baseURL:"https://smartnotesai-7ifv.onrender.com/api",
       headers: { 
         'Content-Type': 'application/json'
      }
